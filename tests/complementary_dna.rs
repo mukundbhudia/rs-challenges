305a3b66133926fb2c8e28707d2f7f71742266f1
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-08-28 11:51:01 +0100

    Completed complementary_dna challenge

---
+++ tests/complementary_dna.rs
@@ -0,0 +1,27 @@
+// From: https://www.codewars.com/kata/554e4a2f232cdd87d9000038/train/rust
+//
+// In DNA strings, symbols "A" and "T" are complements of each other, as 
+// "C" and "G". You have function with one side of the DNA (string,
+// except for Haskell); you need to get the other complementary side.
+// DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+pub fn dna_strand(dna: &str) -> String {
+    dna.chars().map(|x| match x {
+        'A' => 'T',
+        'T' => 'A',
+        'C' => 'G',
+        'G' => 'C',
+        _ => unreachable!(),
+    }).collect()
+}
+
+#[cfg(test)]
+mod shortest_word_tests {
+    use super::*;
+    #[test]
+    fn general_tests() {
+        assert_eq!(dna_strand("AAAA"), "TTTT");
+        assert_eq!(dna_strand("ATTGC"), "TAACG");
+        assert_eq!(dna_strand("GTAT"), "CATA");
+    }
+}
