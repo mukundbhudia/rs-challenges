8b7e89f57fd87c1c14b0507fbe13326728234a1b
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-09-08 10:43:33 +0100

    Completed opposite challenge

---
+++ tests/opposite.rs
@@ -0,0 +1,18 @@
+// From: https://www.codewars.com/kata/56dec885c54a926dcd001095/train/rust
+
+// Very simple, given a number, find its opposite.
+
+
+pub fn opposite(number: i32) -> i32 {
+    number * -1     // Also can do `-number`
+}
+
+#[cfg(test)]
+mod opposite_tests {
+    use super::*;
+    #[test]
+    fn general_tests() {
+        assert_eq!(opposite(1), -1);
+        assert_eq!(opposite(-1), 1);
+    }
+}
