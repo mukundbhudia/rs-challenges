6a22eb7cb343fd5988f9c357b88f6ccf46f88f68
Mukund Bhudia <mukund_bhudia@hotmail.com>
2020-09-08 10:37:03 +0100

    Completed x_and_o challenge

---
+++ tests/x_and_o.rs
@@ -0,0 +1,31 @@
+// From: https://www.codewars.com/kata/55908aad6620c066bc00002a/train/rust
+
+// Check to see if a string has the same amount of 'x's and 'o's.
+// The method must return a boolean and be case insensitive.
+// The string can contain any char.
+
+
+pub fn xo(string: &'static str) -> bool {
+    string.chars().filter(|c| {
+        let x = c.to_lowercase().to_string();
+        x == "x"
+    }).count() ==
+    string.chars().filter(|c| {
+        let o = c.to_lowercase().to_string();
+        o == "o"
+    }).count()
+}
+
+#[cfg(test)]
+mod x_and_o_tests {
+    use super::*;
+    #[test]
+    fn general_tests() {
+        assert_eq!(xo("xo"), true);
+        assert_eq!(xo("Xo"), true);
+        assert_eq!(xo("xxOo"), true);
+        assert_eq!(xo("xxxm"), false);
+        assert_eq!(xo("Oo"), false);
+        assert_eq!(xo("ooom"), false);
+    }
+}
