eed216b723765a33ad83ecdd5a3b2722e23e9d39
Mukund Bhudia <mukund_bhudia@hotmail.com>
2021-05-14 15:01:45 +0100

    feat added get mut method

--- tests/insert_vs_get_mut.rs
+++ tests/insert_vs_get_mut.rs
@@ -43,7 +43,6 @@ pub fn generate_my_records() -> HashMap<String, Record> {
     my_records
 }
 
-
 pub fn check_map_insert(map: &mut HashMap<String, Record>) {
     let second = map.entry("second".to_string()).or_insert(Record {
         name: String::new(),
@@ -54,7 +53,21 @@ pub fn check_map_insert(map: &mut HashMap<String, Record>) {
     second.age = 21;
 }
 
-pub fn check_map_get_mut(map: HashMap<String, Record>) {}
+pub fn check_map_get_mut(map: &mut HashMap<String, Record>) {
+    if let Some(second) = map.get_mut("second") {
+        second.grade = 'A';
+        second.age = 21;
+    } else {
+        map.insert(
+            "key".to_string(),
+            Record {
+                name: String::new(),
+                grade: 'X',
+                age: 0,
+            },
+        );
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -63,6 +76,18 @@ mod tests {
     fn test_map_insert_method() {
         let mut my_records = generate_my_records();
         check_map_insert(&mut my_records);
+        let first = my_records.get("first").unwrap();
+        let second = my_records.get("second").unwrap();
+        assert_eq!(second.grade, 'A');
+        assert_eq!(second.age, 21);
+        assert_eq!(first.grade, 'B');
+        assert_eq!(first.age, 20);
+    }
+
+    #[test]
+    fn test_map_get_mut_method() {
+        let mut my_records = generate_my_records();
+        check_map_get_mut(&mut my_records);
         let first = my_records.get("first").unwrap();
         let second = my_records.get("second").unwrap();
         assert_eq!(second.grade, 'A');
