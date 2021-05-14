f52a75cd940efb0807a78222b1e600c031395eaf
Mukund Bhudia <mukund_bhudia@hotmail.com>
2021-05-14 14:53:58 +0100

    feat: wired up insert version

--- tests/insert_vs_get_mut.rs
+++ tests/insert_vs_get_mut.rs
@@ -1,14 +1,70 @@
 use std::collections::HashMap;
 
-pub fn check_map_insert(map: HashMap<String, String>) {}
+pub struct Record {
+    name: String,
+    grade: char,
+    age: i8,
+}
 
-pub fn check_map_get_mut(map: HashMap<String, String>) {}
+pub fn generate_my_records() -> HashMap<String, Record> {
+    let mut my_records = HashMap::new();
+    my_records.insert(
+        "first".to_string(),
+        Record {
+            name: "Fred".to_string(),
+            grade: 'B',
+            age: 20,
+        },
+    );
+    my_records.insert(
+        "second".to_string(),
+        Record {
+            name: "Bob".to_string(),
+            grade: 'C',
+            age: 20,
+        },
+    );
+    my_records.insert(
+        "third".to_string(),
+        Record {
+            name: "Clara".to_string(),
+            grade: 'A',
+            age: 20,
+        },
+    );
+    my_records.insert(
+        "forth".to_string(),
+        Record {
+            name: "Jane".to_string(),
+            grade: 'B',
+            age: 20,
+        },
+    );
+    my_records
+}
+
+
+pub fn check_map_insert(map: &mut HashMap<String, Record>) {
+    let second = map.entry("second".to_string()).or_insert(Record {
+        name: String::new(),
+        grade: 'X',
+        age: 0,
+    });
+    second.grade = 'A';
+    second.age = 21;
+}
+
+pub fn check_map_get_mut(map: HashMap<String, Record>) {}
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn returns_expected() {
-        assert_eq!(true, false);
+        let mut my_records = generate_my_records();
+        let my_records_expected = check_map_insert(&mut my_records);
+        let second = my_records.get("second").unwrap();
+        assert_eq!(second.grade, 'A');
+        assert_eq!(second.age, 21);
     }
 }
