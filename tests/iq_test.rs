29b6bd64417c3869a6bcf2809a646869a9589993
Mukund Bhudia <mukund_bhudia@hotmail.com>
2021-06-04 17:56:51 +0100

    fix: completed iq test challenge

--- tests/iq_test.rs
+++ tests/iq_test.rs
@@ -1,35 +1,26 @@
 // From: https://www.codewars.com/kata/552c028c030765286c00007d/train/rust
 
 pub fn iq_test(numbers: &str) -> u64 {
-    // match numbers
-    //     .split_whitespace()
-    //     .map(|x| x.parse::<u64>().unwrap())
-    //     .enumerate()
-    //     .find(|(_, x)| x % 2 != 0)
-    // {
-    //     Some((i, _)) => i as u64 + 1,
-    //     None => 0,
-    // }
+    let mut evens = vec![];
+    let mut odds = vec![];
 
-    let digits = numbers
+    for pair in numbers
         .split_whitespace()
-        .map(|x| x.parse::<u64>().unwrap())
+        .filter_map(|x| x.parse::<u64>().ok())
         .enumerate()
-        // .map(|(_, x)| if x % 2 != 0 { true } else { false} )
-        .collect::<Vec<(usize, u64)>>();
-
-    let mut is_prev_odd = digits[0].1 % 2 != 0;
-    for i in 1..digits.len() {
-        let pair = digits[i];
-        let is_current_odd = pair.1 % 2 != 0;
-        println!("{:?}, prev odd: {}, current odd: {}", pair, is_prev_odd, is_current_odd);
-        if is_current_odd != is_prev_odd {
-            return pair.0 as u64 + 1
+    {
+        if pair.1 % 2 != 0 {
+            odds.push(pair);
+        } else {
+            evens.push(pair);
         }
-        is_prev_odd = is_current_odd;
     }
-    println!("{:?}", digits);
-    0
+
+    if evens.len() == 1 {
+        evens.pop().unwrap_or((0, 0)).0 as u64 + 1
+    } else {
+        odds.pop().unwrap_or((0, 0)).0 as u64 + 1
+    }
 }
 
 #[cfg(test)]
